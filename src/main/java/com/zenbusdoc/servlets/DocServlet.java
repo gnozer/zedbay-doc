418c1f10bf179fc586ccaa4a6ce3831af13f2729
JohnDuff44 <sim.vergnaud@gmail.com>
2017-10-31 10:33:06 +0100

    [add] Change the path of the driver.html

--- src/main/java/com/zenbusdoc/servlets/DocServlet.java
+++ src/main/java/com/zenbusdoc/servlets/DocServlet.java
@@ -11,19 +11,21 @@ import javax.servlet.http.HttpServletResponse;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
+import org.omg.Messaging.SyncScopeHelper;
 
 public class DocServlet extends HttpServlet {
 
 	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException{
 
 		Document dom = Jsoup.parse(getServletContext().getResourceAsStream("/index.html"), "UTF-8", req.getRequestURI());
-		
-		File driverSection = new File("../../../../webapp/driver.html");
+
+		File driverSection = new File("/home/simon/Documents/ZENBUS_DEV/ZEDBAY_DOC/src/main/webapp/driver.html");
 		Document section = Jsoup.parse(driverSection, "UTF-8", req.getRequestURI());
 		
 		Element divContent = dom.select("div#content").first();
 		divContent.appendChild(section);
 		
+		resp.setContentType("text/html;charset=UTF-8");
 		resp.getWriter().write(dom.outerHtml());
 	}
 }
